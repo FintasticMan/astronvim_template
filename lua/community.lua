385f8a451528f3952a46d14e7950b71d34c66661
FintasticMan <finlay.neon.kid@gmail.com>
2024-03-23 02:20:21 +0100

    Change colour scheme to Kanagawa

--- lua/community.lua
+++ lua/community.lua
@@ -5,6 +5,7 @@
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
+  { import = "astrocommunity.colorscheme.kanagawa-nvim" },
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.pack.cpp" },
